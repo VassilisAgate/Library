31bab66b04a5515aefd74ea34f242eee4d37b2a0
VassilisAgate <93085288+VassilisAgate@users.noreply.github.com>
2021-11-20 13:57:37 +0200

    Add files via upload

--- Library/src/Core/Borrowable.java
+++ Library/src/Core/Borrowable.java
@@ -1,5 +1,9 @@
 package Core;
 
 public interface Borrowable {
+	public void borrowItem();
+	public void returnItem();
+	public int getId();
+	public boolean isAvailable();
 
 }
--- Library/src/Core/Member.java
+++ Library/src/Core/Member.java
@@ -1,5 +1,60 @@
 package Core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Member {
+
+	private int id;
+	private String name;
+	static final int MAX_ON_LOAN = 6;
+	private List<Borrowable> onLoan;
+
+	public Member(int id, String name) {
+		this.id = id;
+		this.name = name;
+		onLoan = new ArrayList<Borrowable>();
+	}
+
+	public void borrows(Borrowable b) {
+
+		if (onLoan.size() < MAX_ON_LOAN) {
+			b.borrowItem();
+			onLoan.add(b);
+
+		}
+	}
+
+	public void returns(Borrowable b) {
+
+		b.returnItem();
+		onLoan.remove(b);
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static int getMaxOnLoan() {
+		return MAX_ON_LOAN;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", name=" + name + ", MAX_ON_LOAN=" + MAX_ON_LOAN + "]";
+	}
 
 }
