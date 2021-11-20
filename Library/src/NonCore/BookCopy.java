a9ade55501220241edbf75d7a7e2e732099d7178
VassilisAgate <93085288+VassilisAgate@users.noreply.github.com>
2021-11-20 16:55:51 +0200

    Add files via upload

--- Library/src/NonCore/BookCopy.java
+++ Library/src/NonCore/BookCopy.java
@@ -1,13 +1,31 @@
 package NonCore;
 
-public class BookCopy {
-	
+import Core.Borrowable;
+import Exception.AvailabilityException;
+
+public class BookCopy implements Borrowable {
+
 	private int id;
 	private boolean available;
+	private Book book;
 
 	public BookCopy(int id) {
 		this.id = id;
 		this.available = true;
+	}
+
+	public void borrowItem() throws AvailabilityException {
+		if (available == true) {
+			available = false;
+		}
+		else {
+			throw new AvailabilityException("Problem: the book copy cannot be borrowed");
+		}
+	}
+
+	public void returnItem() {
+		if (available == false)
+			available = true;
 	}
 
 	public int getId() {
@@ -30,6 +48,5 @@ public class BookCopy {
 	public String toString() {
 		return "BookCopy [id=" + id + ", available=" + available + "]";
 	}
-	
-	
+
 }
